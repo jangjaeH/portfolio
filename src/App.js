4bbcf11fe36ce395e20e9095730aae64131d844c
장재혁 <pvcgrdo@gmail.com>
2022-02-15 15:54:47 +0900

    :fire:add:layout

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App(){
 
     return (
         <div className='app'>
-            <footer/>
+            <Footer/>
             <header/>
         </div>
     )
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
-function Footer() {
-
-
-    return {
-
-    }
+import './Footer.scss';
+function Footer(props) {
+    return (
+        <div className='footer'>
+            <small>COPYRIGHT (c)2022. jhjang. All rights reserved. </small>
+        </div>
+    )
 }
 
 export default Footer;
