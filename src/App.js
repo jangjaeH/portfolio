88e5d4674212f6a49f6e147ecc99a584572f7e9c
장재혁 <pvcgrdo@gmail.com>
2022-02-15 15:13:46 +0900

    :fire:add:default layout

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from './Components/Footer';
+import header from './Components/Header'
+function App(){
+
+    return (
+        <div className='app'>
+            <footer/>
+            <header/>
+        </div>
+    )
+}
+
+export default App;
