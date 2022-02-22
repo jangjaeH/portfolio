72e8fe5b2ebf89a11ccd8cbccd04339795bb5e44
장재혁 <pvcgrdo@gmail.com>
2022-02-22 15:01:09 +0900

    :rocket:Profile update

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -6,8 +6,10 @@ import {
   ModalHeader,
   ModalCloseButton,
   ModalBody,
-  useDisclosure,
-  Flex,
+  Image,
+  Box,
+  Text,
+  Heading,
 } from "@chakra-ui/react";
 import "./Profile.scss";
 
@@ -16,9 +18,18 @@ function Profile({ isOpen, onClose }) {
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent display="flex" alignItems="center" justifyContent="center" >
-          <ModalHeader w="50%" h="2%">Profile</ModalHeader>
+          <ModalHeader w="50%" h="2%">
           <ModalCloseButton w="3%" h="3%" />
-          <ModalBody w="50%" h="70%" bg="#FFFFFF">안녕하세요</ModalBody>
+          </ModalHeader>
+          <ModalBody w="50%" h="70%" bg="#FFFFFF">
+            <Box align="center">
+              <Image className="profile-image" mt="10px" boxSize="200px" src="./image/image.jpg" alt="my Image"></Image>
+              <Text fontSize="15pt">JaeHyeok Jang</Text>
+            </Box>
+            <Box mt="10px" className="profile-skils" align="center">
+                <Heading size="lg">My Skil</Heading>
+            </Box>
+          </ModalBody>
         </ModalContent>
       </Modal>
   );
